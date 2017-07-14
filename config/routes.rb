74d8123a0b400e6adce0ed421f009e5fdf5997ff
Erik Johnson <mindovermiles262@gmail.com>
2017-07-13 21:41:17 -0700

    users new, create, index, show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "static_pages#index"
 
+  resources :users, only: [:new, :create, :destroy]
+  
 end
